24ba97f63dc40016f00e722989b9ac8550ac8f56
Guy <glevy3@depaul.edu>
2021-04-23 22:35:17 -0500

    Adding usage

--- src/main/java/com/analyzer/Main.java
+++ src/main/java/com/analyzer/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Java Quality Analyzer (c) 2021 - SE433");
         System.out.println("Usage: ");
+        System.out.println("-help - print usage");
+        System.out.println("-input=<source directory> - scan this directory for .java files");
     }
 }
